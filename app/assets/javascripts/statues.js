4a3b8255f023c3e3196dccbe6d1d22c84ca3e376
ricardo <ricardo.rsr.rodrigues@gmail.com>
2013-10-08 16:30:37 +0100

    refactor 2nd docking js file for modularity. add graph and model description divs

--- app/assets/javascripts/statues.js
+++ app/assets/javascripts/statues.js
@@ -1,5 +1,11 @@
 $(function() {
 
+    var x_unit = $(".x_axis_unit").html();
+    var y_unit = $(".y_axis_unit").html();
+    
+    
+    var model, prediction, linear_m = 10, linear_b = 0, quadratic_k = 1, quadratic_h= 0, quadratic_b =0, cubic_k = 1, cubic_h= 0, cubic_b =0;;
+
     $(".background").on("click", ".background_next", function() {
 	 $(".background").slideUp();
 	 $(".strategy").slideDown();
@@ -22,7 +28,147 @@ $(function() {
 
     $(".data-gathering").on("click", ".data-gathering_next", function() {
 	 $(".data-gathering").slideUp();
-	 //$(".").slideDown();
+	 $(".model-choice").slideDown();
     });
 
+
+    $(".model-choice").on("click", ".model-choice_previous", function() {
+	 $(".model-choice").slideUp();
+	 $(".data-gathering").slideDown();
+    });
+
+
+    $(".model_slider_controls").slider();
+
+
+
+
+
+    $(".model_choice_buttons").on("click", ".linear_model_button", function() {
+	 $(".model-choice_buttons_explanation").hide();
+	 $(".model_function_controls").hide();
+	 $(".linear_function_controls").show();
+	 $(".model_choice_model_information_div").show();
+
+	 model = 1;
+	 
+	 if (linear_m) {
+	     if (linear_b > 0)
+		  $(".model_choice_model_formula").html(y_unit+" = "+linear_m+" "+x_unit +" + "+linear_b);
+	     else if (linear_b < 0)
+		  $(".model_choice_model_formula").html(y_unit+" = "+linear_m+" "+x_unit +" - "+linear_b*(-1));
+	     else
+		  $(".model_choice_model_formula").html(y_unit+" = "+linear_m+" "+x_unit);
+	 }
+	 else 
+	     $(".model_choice_model_formula").html(y_unit+" = "+linear_b);
+
+
+    });
+
+    $(".model_choice_buttons").on("click", ".quadratic_model_button", function() {
+	 $(".model-choice_buttons_explanation").hide();
+	 $(".model_function_controls").hide();
+	 $(".quadratic_function_controls").show();
+	 $(".model_choice_model_information_div").show();
+	 model = 2;
+	 
+	 if (quadratic_k) {
+	     if (quadratic_h > 0) {
+		  if (quadratic_b > 0) {
+		      $(".model_choice_model_formula").html(y_unit+" = "+quadratic_k+" ("+x_unit+"+"+quadratic_h+")<sup>2</sup> + "+quadratic_b);
+		  }
+		  else if (quadratic_b < 0) {
+		      $(".model_choice_model_formula").html(y_unit+" = "+quadratic_k+" ("+x_unit+"+"+quadratic_h+")<sup>2</sup> - "+quadratic_b*(-1));
+		  }
+		  else {
+		      $(".model_choice_model_formula").html(y_unit+" = "+quadratic_k+" ("+x_unit+"+"+quadratic_h+")<sup>2</sup>");
+		  }
+	     }
+	     else if (quadratic_h < 0) {
+		  if (quadratic_b > 0) {
+		      $(".model_choice_model_formula").html(y_unit+" = "+quadratic_k+" ("+x_unit+"-"+quadratic_h*(-1)+")<sup>2</sup> + "+quadratic_b);
+		  }
+		  else if (quadratic_b < 0) {
+		      $(".model_choice_model_formula").html(y_unit+" = "+quadratic_k+" ("+x_unit+"-"+quadratic_h*(-1)+")<sup>2</sup> - "+quadratic_b*(-1));
+		  }
+		  else {
+		      $(".model_choice_model_formula").html(y_unit+" = "+quadratic_k+" ("+x_unit+"-"+quadratic_h*(-1)+")<sup>2</sup>");
+		  }
+	     }
+	     else {
+		  if (quadratic_b > 0) {
+		      $(".model_choice_model_formula").html(y_unit+" = "+quadratic_k+" "+x_unit+"<sup>2</sup> + "+quadratic_b);
+		  }
+		  else if (quadratic_b < 0) {
+		      $(".model_choice_model_formula").html(y_unit+" = "+quadratic_k+" "+x_unit+"<sup>2</sup> - "+(-1)*quadratic_b);
+		  }
+		  else {
+		      $(".model_choice_model_formula").html(y_unit+" = "+quadratic_k+" "+x_unit+"<sup>2</sup>");
+		  }
+	     }
+
+	 }
+	 else  {
+	     if (quadratic_b) {
+		  $(".model_choice_model_formula").html(y_unit+" = "+quadratic_b);
+	     }
+	     else {
+		  $(".model_choice_model_formula").html(y_unit+" = 0");
+	     }
+	 }
+    });
+
+    $(".model_choice_buttons").on("click", ".cubic_model_button", function() {
+	 $(".model-choice_buttons_explanation").hide();
+	 $(".model_function_controls").hide();
+	 $(".cubic_function_controls").show();
+	 $(".model_choice_model_information_div").show();
+	 model = 3;
+	 
+	 if (cubic_k) {
+	     if (cubic_h > 0) {
+		  if (cubic_b > 0) {
+		      $(".model_choice_model_formula").html(y_unit+" = "+cubic_k+" ("+x_unit+"+"+cubic_h+")<sup>3</sup> + "+cubic_b);
+		  }
+		  else if (cubic_b < 0) {
+		      $(".model_choice_model_formula").html(y_unit+" = "+cubic_k+" ("+x_unit+"+"+cubic_h+")<sup>3</sup> - "+cubic_b*(-1));
+		  }
+		  else {
+		      $(".model_choice_model_formula").html(y_unit+" = "+cubic_k+" ("+x_unit+"+"+cubic_h+")<sup>3</sup>");
+		  }
+	     }
+	     else if (cubic_h < 0) {
+		  if (cubic_b > 0) {
+		      $(".model_choice_model_formula").html(y_unit+" = "+cubic_k+" ("+x_unit+"-"+cubic_h*(-1)+")<sup>3</sup> + "+cubic_b);
+		  }
+		  else if (cubic_b < 0) {
+		      $(".model_choice_model_formula").html(y_unit+" = "+cubic_k+" ("+x_unit+"-"+cubic_h*(-1)+")<sup>3</sup> - "+cubic_b*(-1));
+		  }
+		  else {
+		      $(".model_choice_model_formula").html(y_unit+" = "+cubic_k+" ("+x_unit+"-"+cubic_h*(-1)+")<sup>3</sup>");
+		  }
+	     }
+	     else {
+		  if (cubic_b > 0) {
+		      $(".model_choice_model_formula").html(y_unit+" = "+cubic_k+" "+x_unit+"<sup>3</sup> + "+cubic_b);
+		  }
+		  else if (cubic_b < 0) {
+		      $(".model_choice_model_formula").html(y_unit+" = "+cubic_k+" "+x_unit+"<sup>3</sup> - "+(-1)*cubic_b);
+		  }
+		  else {
+		      $(".model_choice_model_formula").html(y_unit+" = "+cubic_k+" "+x_unit+"<sup>3</sup>");
+		  }
+	     }
+
+	 }
+	 else  {
+	     if (cubic_b) {
+		  $(".model_choice_model_formula").html(y_unit+" = "+cubic_b);
+	     }
+	     else {
+		  $(".model_choice_model_formula").html(y_unit+" = 0");
+	     }
+	 }
+    });
 });
